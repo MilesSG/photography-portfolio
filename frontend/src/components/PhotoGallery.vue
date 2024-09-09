503645f3b8e8abf4f4e68b9b802af8cbd1e2117c
miles <1290672153@qq.com>
2024-09-09 16:42:33 +0800

    调整标题布局和时间轴样式，改善页面显示效果

--- frontend/src/components/PhotoGallery.vue
+++ frontend/src/components/PhotoGallery.vue
@@ -11,10 +11,14 @@
         <!-- 厦门照片展示 -->
         <el-timeline-item v-if="xiamenPhotos.length" placement="top">
           <template #dot>
-            <div class="timeline-dot"></div>
+            <!-- 使用图标或颜色来区分节点 -->
+            <i class="iconfont icon-xiamen timeline-dot"></i>
           </template>
-          <!-- 地点和时间 -->
-          <h2 class="timeline-header">厦门 2024年6月</h2>
+          <!-- 单独展示时间和地点，放在左侧 -->
+          <div class="timeline-info">
+            <div class="timeline-label">厦门</div>
+            <div class="timeline-date">2024年6月</div>
+          </div>
           <div class="gallery">
             <div v-for="(photo, index) in xiamenPhotos" :key="'xm' + index" class="photo-card">
               <img :src="photo.thumbnail" alt="Xiamen Photo" @click="openModal(photo, index, 'xiamenPhotos')" />
@@ -25,10 +29,12 @@
         <!-- 重庆照片展示 -->
         <el-timeline-item v-if="chongqingPhotos.length" placement="top">
           <template #dot>
-            <div class="timeline-dot"></div>
+            <i class="iconfont icon-chongqing timeline-dot" style="color: #67C23A;"></i>
           </template>
-          <!-- 地点和时间 -->
-          <h2 class="timeline-header">重庆 2024年6月</h2>
+          <div class="timeline-info">
+            <div class="timeline-label">重庆</div>
+            <div class="timeline-date">2024年6月</div>
+          </div>
           <div class="gallery">
             <div v-for="(photo, index) in chongqingPhotos" :key="'cq' + index" class="photo-card">
               <img :src="photo.thumbnail" alt="Chongqing Photo" @click="openModal(photo, index, 'chongqingPhotos')" />
@@ -39,10 +45,12 @@
         <!-- 新西兰照片展示 -->
         <el-timeline-item v-if="newZealandPhotos.length" placement="top">
           <template #dot>
-            <div class="timeline-dot"></div>
+            <i class="iconfont icon-newzealand timeline-dot" style="color: #E6A23C;"></i>
           </template>
-          <!-- 地点和时间 -->
-          <h2 class="timeline-header">新西兰 2024年5月</h2>
+          <div class="timeline-info">
+            <div class="timeline-label">新西兰</div>
+            <div class="timeline-date">2024年5月</div>
+          </div>
           <div class="gallery">
             <div v-for="(photo, index) in newZealandPhotos" :key="'nz' + index" class="photo-card">
               <img :src="photo.thumbnail" alt="New Zealand Photo" @click="openModal(photo, index, 'newZealandPhotos')" />
@@ -53,10 +61,12 @@
         <!-- 香港机场转机照片展示 -->
         <el-timeline-item v-if="HKAirportPhotos.length" placement="top">
           <template #dot>
-            <div class="timeline-dot"></div>
+            <i class="iconfont icon-hkairport timeline-dot" style="color: #F56C6C;"></i>
           </template>
-          <!-- 地点和时间 -->
-          <h2 class="timeline-header">香港机场 2024年4月</h2>
+          <div class="timeline-info">
+            <div class="timeline-label">香港机场</div>
+            <div class="timeline-date">2024年4月</div>
+          </div>
           <div class="gallery">
             <div v-for="(photo, index) in HKAirportPhotos" :key="'hk' + index" class="photo-card">
               <img :src="photo.thumbnail" alt="HKAirport Photo" @click="openModal(photo, index, 'HKAirportPhotos')" />
@@ -67,10 +77,12 @@
         <!-- 新加坡照片展示 -->
         <el-timeline-item v-if="singaporePhotos.length" placement="top">
           <template #dot>
-            <div class="timeline-dot"></div>
+            <i class="iconfont icon-singapore timeline-dot" style="color: #909399;"></i>
           </template>
-          <!-- 地点和时间 -->
-          <h2 class="timeline-header">新加坡 2024年3月</h2>
+          <div class="timeline-info">
+            <div class="timeline-label">新加坡</div>
+            <div class="timeline-date">2024年3月</div>
+          </div>
           <div class="gallery">
             <div v-for="(photo, index) in singaporePhotos" :key="'sg' + index" class="photo-card">
               <img :src="photo.thumbnail" alt="Singapore Photo" @click="openModal(photo, index, 'singaporePhotos')" />
@@ -183,14 +195,22 @@ export default {
 
 <style scoped>
 .photo-gallery {
-  display: flex;
+  display: block; /* 将布局改为块级元素 */
   padding: 20px;
 }
 
+.header {
+  width: 100%; /* 让标题部分占据整行 */
+  text-align: center; /* 标题内容居中 */
+  margin-bottom: 40px; /* 增加标题与内容之间的间距 */
+}
+
 .timeline-container {
-  flex: 1;
-  padding-right: 20px;
-  position: relative; /* 添加相对定位 */
+  position: relative;
+  display: flex; /* 使用flex布局，使时间轴和内容并排显示 */
+  flex-direction: column; /* 使时间轴垂直排列 */
+  align-items: flex-start; /* 左对齐时间轴内容 */
+  padding-left: 180px; /* 增加左边距以避免时间和地点信息与标题重叠 */
 }
 
 .gallery {
@@ -210,23 +230,59 @@ export default {
   transform: scale(1.05);
 }
 
+.iconfont {
+  font-size: 20px;
+  margin-right: 8px;
+}
+
+.icon-xiamen {
+  color: #409eff;
+}
+
+.icon-chongqing {
+  color: #67C23A;
+}
+
+.icon-newzealand {
+  color: #E6A23C;
+}
+
+.icon-hkairport {
+  color: #F56C6C;
+}
+
+.icon-singapore {
+  color: #909399;
+}
+
 .timeline-dot {
   width: 12px;
   height: 12px;
   border-radius: 50%;
-  background-color: #409eff;
   position: relative;
   top: -10px;
   left: -6px;
 }
 
-.timeline-header {
-  position: absolute; /* 绝对定位 */
-  left: 25px; /* 根据需要调整位置 */
-  margin: 0;
-  padding: 10px 0;
-  font-size: 30px; /* 调整字体大小 */
+.timeline-info {
+  position: absolute;
+  left: -140px; /* 调整这个值来控制时间和地点的位置 */
+  top: 20px;
+  text-align: right;
+}
+
+.timeline-label {
+  font-size: 24px;
   font-weight: bold;
   color: #333;
 }
+
+.timeline-date {
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 10px;
+}
+
+
+
 </style>
